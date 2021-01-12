61a7fe2143b9d71cdb1ec898d7d6bab1ef16a00a
jeyajeevithaasri <indirasivakumar881@gmail.com>
2021-01-12 16:49:13 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -17,7 +17,6 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -35,7 +34,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+   platform = new Ground(150,300,300,170)
 }
 
 function draw(){
